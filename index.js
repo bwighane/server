2040a3fd8292a833f6214a9571d6669aec80d704
ibwighane <bsmwalwanda@gmail.com>
2018-10-24 20:46:04 +0200

    fully fledged server

---
+++ index.js
@@ -0,0 +1,16 @@
+"use strict";
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 7000;
+
+app.use(cors());
+app.get("/", (req, res) =>
+  res.json({
+    name: "Bwigahne",
+    surname: "Mwalwanda",
+    middleName: "Clive"
+  })
+);
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
