2a364573e5b428a2114ea00af0292ce4e33dfd25
ibwighane <bsmwalwanda@gmail.com>
2018-10-29 16:20:30 +0200

    no cors

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const app = express();
 const port = 10000;
 
-app.use(cors());
+// app.use(cors());
 app.get("/test-channel", (req, res) => res.json({ name: "cors" }));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
