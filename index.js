c091ff4ff12ac86755ca62f97e0c99a194cbf1e7
ibwighane <bsmwalwanda@gmail.com>
2018-10-29 16:05:17 +0200

    dnnd

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
-"use strict";
 const express = require("express");
-const cors = require("cors");
 const app = express();
-const port = 7000;
+const port = 10000;
 
-// app.use(cors());
-app.get("/", (req, res) =>
-  res.json({
-    name: "Bwigahne",
-    surname: "Mwalwanda",
-    middleName: "Clive"
-  })
-);
+app.get("/", (req, res) => res.send("Hello World!"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
