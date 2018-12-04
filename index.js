f949e983ab6397dbeafd2b3df716136046d77cca
ibwighane <bsmwalwanda@gmail.com>
2018-12-04 12:59:49 +0200

    real ip

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const cors = require("cors");
 const app = express();
 const requestIp = require('request-ip')
 
-const port = 10000;
+const port = 11000;
 app.use(cors());
 app.get("/", (req, res) => {
-  const clientIp = requestIp.getClientIp(req);
-  res.json({ ip: clientIp });
+  const clientIp = requestIp.getClientIp(req).split(":");
+  console.log(requestIp)
+  res.json({ ip: clientIp[clientIp.length - 1] });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
